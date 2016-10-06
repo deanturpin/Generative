8691de7cc42e46923a5d1da487c8a62e2c9105e6
deanturpin <git@lvbtn.uk>
2016-10-06 08:01:16 +0100

    print chars

--- maze.cpp
+++ maze.cpp
@@ -8,18 +8,18 @@ int main() {
 
 	cout << "Maze" << endl;
 
-	const int X = 40;
-	const int Y = 30;
+	const int X = 80;
+	const int Y = 40;
 
 	// Create maze
-	vector<unsigned int> raster(X, 'X');
-	vector<vector<unsigned int>> maze(Y, raster);
+	vector<char> raster(X, '.');
+	vector<vector<char>> maze(Y, raster);
 
 	// Print maze
 	for_each(maze.cbegin(), maze.cend(), [](auto &r) {
 
 		for_each(r.cbegin(), r.cend(), [](auto &b) { cout << b; });
-		
+
 		cout << endl;
 	});
 
