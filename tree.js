526f0675bc18fcbf136efffc6c7644d6683bd7a6
deanturpin <git@lvbtn.uk>
2016-11-03 22:35:05 +0000

    add new branches with a bit of jitter

--- tree.js
+++ tree.js
@@ -15,7 +15,26 @@ function node(x, y) {
 nodes = []
 nodes[nodes.length] = new node(0, 0)
 
+jitter = 1
+for (i = 0; i < 5; ++i) {
+
+	totalNodes = nodes.length
+
+	// Iterate over nodes adding new branches
+	for (n = 0; n < totalNodes; ++n) {
+
+		// New left branch
+		x = nodes[n].x - 1 - (jitter * Math.random())
+		y = nodes[n].y + 3 + (jitter * Math.random())
+		nodes[nodes.length] = new node(x, y)
+
+		// New right branch
+		x = nodes[n].x + 1 + (jitter * Math.random())
+		y = nodes[n].y + 3 + (jitter * Math.random())
+		nodes[nodes.length] = new node(x, y)
+	}
+}
+
 // Dump all points
-console.log("size of array", nodes.length)
 for (n = 0; n < nodes.length; ++n)
 	nodes[n].print()
