fb67b188becc18f2f1c88dcc0ce38ccefbc07093
deanturpin <git@lvbtn.uk>
2016-11-03 18:35:22 +0000

    added print function to class

--- tree.js
+++ tree.js
@@ -3,12 +3,22 @@
 // A node
 function Node(x, y) {
 
+	// Position
 	this.X = x;
 	this.Y = y;
+
+	// Debug
+	this.print = function() {
+
+		console.log(x + ", " + y + ",")
+	}
 }
 
 // Create first node
 nodes = []
 nodes[nodes.length] = new Node(0, 0)
 
+// Dump all points
 console.log("size of array", nodes.length)
+for (n = 0; n < nodes.length; ++n)
+	nodes[n].print()
