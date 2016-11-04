c695cbe2e0469ccd2384906a215e5b5f45ec075c
deanturpin <git@lvbtn.uk>
2016-11-04 16:46:24 +0000

    add some randomness

--- tree.js
+++ tree.js
@@ -1,28 +1,34 @@
 #!/usr/bin/node
 
 // A node
-function node(x, y, d) {
+function node(x, y, d, l) {
 
 	// Position
 	this.x = x;
 	this.y = y;
 	this.direction = d;
+	this.length = l;
 
 	// Debug
 	this.print = function() {
 		
-		console.log(this.x + ", " + this.y + "," + this.direction / radians)
+		console.log(
+			this.x,
+			this.y,
+			this.direction / radians,
+			this.length
+		)
 	}
 }
 
 // Branch properties
-const length = 5
 const radians = 2 * Math.PI / 360
-const angle = 10 * radians
+const angle = 20 * radians
+const scale = 0.9
 
 // Create first node at the origin pointing upwards
 nodes = []
-nodes[nodes.length] = new node(0.0, 0.0, 90 * radians)
+nodes[nodes.length] = new node(0.0, 0.0, 90 * radians, 5)
 
 for (i = 0; i < 5; ++i) {
 
@@ -33,15 +39,15 @@ for (i = 0; i < 5; ++i) {
 
 		// New clockwise branch
 		direction = nodes[n].direction - angle
-		x = nodes[n].x + (length * Math.cos(direction))
-		y = nodes[n].y + (length * Math.sin(direction))
-		nodes[nodes.length] = new node(x, y, direction)
+		x = nodes[n].x + (nodes[n].length * Math.cos(direction))
+		y = nodes[n].y + (nodes[n].length * Math.sin(direction))
+		nodes[nodes.length] = new node(x, y, direction, nodes[n].length * Math.random())
 
 		// New anticlockwise branch
 		direction = nodes[n].direction + angle
-		x = nodes[n].x + (length * Math.cos(direction))
-		y = nodes[n].y + (length * Math.sin(direction))
-		nodes[nodes.length] = new node(x, y, direction)
+		x = nodes[n].x + (nodes[n].length * Math.cos(direction))
+		y = nodes[n].y + (nodes[n].length * Math.sin(direction))
+		nodes[nodes.length] = new node(x, y, direction, nodes[n].length * Math.random())
 	}
 }
 
