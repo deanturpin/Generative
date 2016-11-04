ee9355e95628ddcec566e96495a45f5ef276fc0e
deanturpin <git@lvbtn.uk>
2016-11-04 17:11:16 +0000

    hide the maths in the class

--- tree.js
+++ tree.js
@@ -8,6 +8,8 @@ function node(x, y, d, l) {
 	this.y = y;
 	this.direction = d;
 	this.length = l;
+	this.angle = 20
+	this.radians = 2 * Math.PI / 360
 
 	// Debug
 	this.print = function() {
@@ -15,22 +17,34 @@ function node(x, y, d, l) {
 		console.log(
 			this.x,
 			this.y,
-			this.direction / radians,
-			this.length
+			this.direction,
+			this.length,
+			this.angle
 		)
+	}
+
+	this.branch = function(sway) {
+
+		// Direction is old direction plus a sway
+		const direction = this.direction + (this.angle * sway)
+
+		// Trig
+		var x = this.x + (this.length * Math.cos(direction * this.radians))
+		var y = this.y + (this.length * Math.sin(direction * this.radians))
+
+		// Return new node
+		return new node(x, y, direction, this.length)
 	}
 }
 
 // Branch properties
-const radians = 2 * Math.PI / 360
-const angle = 20 * radians
 const scale = 0.9
 
 // Create first node at the origin pointing upwards
 nodes = []
-nodes[nodes.length] = new node(0.0, 0.0, 90 * radians, 5)
+nodes[nodes.length] = new node(0.0, 0.0, 90, 5)
 
-for (i = 0; i < 5; ++i) {
+for (i = 0; i < 2; ++i) {
 
 	totalNodes = nodes.length
 
@@ -38,16 +52,10 @@ for (i = 0; i < 5; ++i) {
 	for (n = 0; n < totalNodes; ++n) {
 
 		// New clockwise branch
-		direction = nodes[n].direction - angle
-		x = nodes[n].x + (nodes[n].length * Math.cos(direction))
-		y = nodes[n].y + (nodes[n].length * Math.sin(direction))
-		nodes[nodes.length] = new node(x, y, direction, nodes[n].length * Math.random())
+		nodes[nodes.length] = nodes[n].branch(1)
 
 		// New anticlockwise branch
-		direction = nodes[n].direction + angle
-		x = nodes[n].x + (nodes[n].length * Math.cos(direction))
-		y = nodes[n].y + (nodes[n].length * Math.sin(direction))
-		nodes[nodes.length] = new node(x, y, direction, nodes[n].length * Math.random())
+		nodes[nodes.length] = nodes[n].branch(-1)
 	}
 }
 
