54c8eafcbd486c1135f1ab6cb3328a0d97569859
deanturpin <git@lvbtn.uk>
2016-11-05 10:24:50 +0000

    add grow routine

--- tree.js
+++ tree.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
+const radians = 2 * Math.PI / 360
+
 // A node
-function node(x1, y1, x2, y2, d, l) {
+function node(x, y, d, l, a) {
 
 	// Position
-	this.x1 = x1;
-	this.y1 = y1;
-	this.x2 = x2;
-	this.y2 = y2;
+	this.x1 = x;
+	this.y1 = y;
+	this.x2 = 0;
+	this.y2 = 0;
 	this.direction = d;
 	this.length = l;
-	this.angle = 20
-	this.radians = 2 * Math.PI / 360
+	this.angle = a
 
 	// Debug
 	this.print = function() {
@@ -31,37 +32,45 @@ function node(x1, y1, x2, y2, d, l) {
 		console.log()
 	}
 
+	// Grow this node
+	this.grow = function() {
+
+		// Trig
+		this.x2 = this.x1 + (this.length * Math.cos(this.direction * radians))
+		this.y2 = this.y1 + (this.length * Math.sin(this.direction * radians))
+	}
+
+	// Branch this node
 	this.branch = function(sway) {
 
 		// Direction is old direction plus a sway
 		const direction = this.direction + (this.angle * sway)
 
-		// Trig
-		var x3 = this.x2 + (this.length * Math.cos(direction * this.radians))
-		var y3 = this.y2 + (this.length * Math.sin(direction * this.radians))
-
 		// Return new node
-		return new node(this.x2, this.y2, x3, y3, direction, this.length * (0.6 * Math.random()))
+		var n = new node(this.x2, this.y2,
+			direction, this.length * 0.9, this.angle * 0.7)
+
+		// And grow it
+		n.grow()
+
+		return n
 	}
 }
 
-// Branch properties
-const scale = 0.9
-
 // Create first node at the origin pointing upwards
 nodes = []
-nodes[nodes.length] = new node(0, 0, 0, 5, 90, 5)
+nodes[nodes.length] = new node(0, 0, 90, 5, 40)
+nodes[0].grow()
 
-for (i = 0; i < 3; ++i) {
+for (i = 0; i < 1; ++i) {
 
 	totalNodes = nodes.length
 
 	// Iterate over nodes adding new branches
 	for (n = 0; n < totalNodes; ++n) {
 
-		// New branches
+		// Branch node
 		nodes[nodes.length] = nodes[n].branch(1)
-		nodes[nodes.length] = nodes[n].branch(0.1)
 		nodes[nodes.length] = nodes[n].branch(-1)
 	}
 }
