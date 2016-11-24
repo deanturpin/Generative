75ac124c0a841b022c6c2488060c6197185a84b5
deanturpin <git@lvbtn.uk>
2016-11-24 17:09:39 +0000

    move canvas/context init to top level

--- js/timestables.js
+++ js/timestables.js
@@ -1,22 +1,9 @@
 "use strict"
 
-onload = function() {
+// Start animation
+function draw(canvas, context) {
 
-	// Create a canvas element
-	var canvas = document.createElement("canvas")
-	var body = document.getElementsByTagName("body")[0]
-
-	// Add it to the DOM
-	body.appendChild(canvas)
-
-	// Define canvas
-	var context = canvas.getContext("2d")
-
-	// Set canvas to window size
-	context.canvas.width = window.innerWidth
-	context.canvas.height = window.innerHeight - 50
-
-	// Place in the middle of the canvas
+	// Find the centre of the canvas
 	const x = canvas.width / 2
 	const y = canvas.height / 2
 
@@ -28,6 +15,33 @@ onload = function() {
 
 	// Iterations
 	const iterations = modulus / increment
+
+	// Draw spokes
+	function spokes(count, multiplier) {
+
+		// Check if we need to do anything
+		if (count > 0) {
+
+			const radius = y * .9
+
+			// Start point - on the perimeter
+			const x2 = x + radius * Math.cos(radians * count / modulus)
+			const y2 = y + radius * Math.sin(radians * count / modulus)
+
+			// End point - a bit further around the perimeter
+			const x3 = x + radius * Math.cos(radians * (count * multiplier) / modulus)
+			const y3 = y + radius * Math.sin(radians * (count * multiplier) / modulus)
+
+			// Draw the line
+			context.beginPath()
+			context.moveTo(x2, y2)
+			context.lineTo(x3, y3)
+			context.stroke()
+
+			// Next spoke
+			spokes(count - 1, multiplier + 1 / modulus)
+		}
+	}
 
 	// Draw frames
 	function frames(iteration, multiplier) {
@@ -42,33 +56,6 @@ onload = function() {
 
 			// Schedule the next frame
 			setTimeout(frames, 10, iteration - 1, multiplier + increment)
-		}
-	}
-
-	// Draw spokes
-	function spokes(count, multiplier) {
-
-		if (count > 0) {
-
-			const radius = y * .9
-			const radians = 2 * Math.PI
-
-			// Start point
-			const x2 = x + radius * Math.cos(radians * count / modulus)
-			const y2 = y + radius * Math.sin(radians * count / modulus)
-
-			// End point
-			const x3 = x + radius * Math.cos(radians * (count * multiplier) / modulus)
-			const y3 = y + radius * Math.sin(radians * (count * multiplier) / modulus)
-
-			// Draw the line
-			context.beginPath()
-			context.moveTo(x2, y2)
-			context.lineTo(x3, y3)
-			context.stroke()
-
-			// Next spoke
-			spokes(count - 1, multiplier + 1/modulus)
 		}
 	}
 
