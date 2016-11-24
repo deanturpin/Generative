5a32d24f9e5685bd00087407b35debe3b139462d
deanturpin <git@lvbtn.uk>
2016-11-24 12:40:50 +0000

    build canvas element using javascript

--- js/timestables.js
+++ js/timestables.js
@@ -1,66 +1,73 @@
 "use strict"
 
-// Define canvas
-var canvas = document.getElementById("canvas")
-var context = canvas.getContext("2d")
+onload = function() {
 
-// Set canvas to window size
-context.canvas.width = window.innerWidth
-context.canvas.height = window.innerHeight
+	var canvas = document.createElement("canvas")
+	var body = document.getElementsByTagName("body")[0]
+	body.appendChild(canvas)
 
-// Place in the middle of the canvas
-const x = canvas.width / 2
-const y = canvas.height / 2
+	// Define canvas
+	// var canvas = document.getElementById("canvas")
+	var context = canvas.getContext("2d")
 
-// Control points around the circle
-const modulus = 200
+	// Set canvas to window size
+	context.canvas.width = window.innerWidth
+	context.canvas.height = window.innerHeight
 
-// Size of increment for each frame
-const increment = .1
+	// Place in the middle of the canvas
+	const x = canvas.width / 2
+	const y = canvas.height / 2
 
-// Iterations
-const iterations = modulus / increment
+	// Control points around the circle
+	const modulus = 200
 
-function frames(iteration, table) {
+	// Size of increment for each frame
+	const increment = .1
 
-	if (iteration > 0) {
+	// Iterations
+	const iterations = modulus / increment
 
-		// Clear screen
-		context.clearRect(0, 0, canvas.width, canvas.height)
+	function frames(iteration, table) {
 
-		// Draw the spokes
-		spokes(modulus, table)
+		if (iteration > 0) {
 
-		// Schedule the next frame
-		setTimeout(frames, 10, iteration - 1, table + increment)
+			// Clear screen
+			context.clearRect(0, 0, canvas.width, canvas.height)
+
+			// Draw the spokes
+			spokes(modulus, table)
+
+			// Schedule the next frame
+			setTimeout(frames, 10, iteration - 1, table + increment)
+		}
 	}
-}
 
-function spokes(count, table) {
+	function spokes(count, table) {
 
-	if (count > 0) {
+		if (count > 0) {
 
-		const radius = y * .9
-		const radians = 2 * Math.PI
+			const radius = y * .9
+			const radians = 2 * Math.PI
 
-		// Start point
-		const x2 = x + radius * Math.cos(radians * count / modulus)
-		const y2 = y + radius * Math.sin(radians * count / modulus)
+			// Start point
+			const x2 = x + radius * Math.cos(radians * count / modulus)
+			const y2 = y + radius * Math.sin(radians * count / modulus)
 
-		// End point
-		const x3 = x + radius * Math.cos(radians * (count * table) / modulus)
-		const y3 = y + radius * Math.sin(radians * (count * table) / modulus)
+			// End point
+			const x3 = x + radius * Math.cos(radians * (count * table) / modulus)
+			const y3 = y + radius * Math.sin(radians * (count * table) / modulus)
 
-		// Draw the line
-		context.beginPath()
-		context.moveTo(x2, y2)
-		context.lineTo(x3, y3)
-		context.stroke()
+			// Draw the line
+			context.beginPath()
+			context.moveTo(x2, y2)
+			context.lineTo(x3, y3)
+			context.stroke()
 
-		// Next spoke
-		spokes(count - 1, table + 1/modulus)
+			// Next spoke
+			spokes(count - 1, table + 1/modulus)
+		}
 	}
-}
 
-// Draw the first frame
-frames(iterations + 1, 1)
+	// Draw the first frame
+	frames(iterations + 1, 1)
+}
