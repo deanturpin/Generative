b03b9e6462a232daff8e10a203263e8da1f92238
deanturpin <git@lvbtn.uk>
2016-11-24 09:27:50 +0000

    make times table external

---
+++ js/timestables.js
@@ -0,0 +1,59 @@
+"use strict"
+
+// Define canvas
+var canvas = document.getElementById("canvas")
+var context = canvas.getContext("2d")
+context.canvas.width = window.innerWidth
+context.canvas.height = window.innerHeight
+
+// Place in the middle of the canvas
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const modulus = 200
+
+function frame(iteration, table) {
+
+	if (iteration > 0) {
+
+		// Clear screen
+		context.clearRect(0, 0, canvas.width, canvas.height)
+
+		// Draw the spokes
+		spoke(modulus, table)
+
+		// Schedule the next frame
+		setTimeout(function() {
+			frame(iteration - 1, table + .02)
+		}, 10)
+	}
+}
+
+function spoke(count, table) {
+
+	if (count > 0) {
+
+		const radius = y * .9
+		const radians = 2 * Math.PI
+
+		// Start point
+		const x2 = x + radius * Math.cos(radians * count / modulus)
+		const y2 = y + radius * Math.sin(radians * count / modulus)
+
+		// End point
+		const x3 = x + radius * Math.cos(radians * (count * table) / modulus)
+		const y3 = y + radius * Math.sin(radians * (count * table) / modulus)
+
+		// Draw the line
+		context.beginPath()
+		context.moveTo(x2, y2)
+		context.lineTo(x3, y3)
+		context.stroke()
+
+		// Next spoke
+		spoke(count - 1, table + 1/modulus)
+	}
+}
+
+// Draw the first frame
+frame(10000, 1)
