2f152579fb284ac4cdda7d46ec3c5295b8772675
deanturpin <git@lvbtn.uk>
2016-11-29 19:23:51 +0000

    inverse. remove commented code

--- buddhabrot/brot.js
+++ buddhabrot/brot.js
@@ -1,7 +1,5 @@
 "use strict"
 
-console.log(new Date)
-
 onload = function() {
 
 	// Create a canvas element
@@ -18,7 +16,7 @@ onload = function() {
 	const scale = 1
 	const width = 400
 	const height = 400
-	const zoom = 200
+	var zoom = 10
 	const iterations = 20
 	context.canvas.width = width * scale
 	context.canvas.height = height * scale
@@ -55,18 +53,14 @@ onload = function() {
 			for (var i = 0; i < iterations; ++i) {
 
 				// Don't look any further if we've escaped the set
-				if ((zr * zr + zi * zi) > 4) {
-
-					console.log("escaped with", iterations - i)
+				if ((zr * zr + zi * zi) > 4)
 					return iterations - i
-				}
 
 				// Calculate next point
 				const zr2 = (zr * zr) + (zi * zi * -1) + cr
 				const zi2 = (zi * zr) + (zr * zi) + ci
 
-				console.log(zr, zi, zr2, zi2, i)
-
+				// Copy the latest
 				zr = zr2
 				zi = zi2
 			}
@@ -74,28 +68,28 @@ onload = function() {
 			return 0
 		}
 
-		console.log = function() {}
-
 		// Test if each element in the bitmap is a member of the set
 		for (var x = 0; x < width; ++x)
 			for (var y = 0; y < height; ++y)
-				bitmap[x][y] = member(x / zoom, y / zoom, iterations)
+				bitmap[x][y] = member(
+					(x - width / 2) / zoom,
+					(y - height / 2) / zoom,
+					iterations)
 
 		// Display 'brot
-		for (var x = 0; x < width; ++x) {
-
-			// console.log(bitmap[x])
-
-			for (var y = 0; y < height; ++y) {
-
-				if (bitmap[x][y] > 0) {
+		for (var x = 0; x < width; ++x)
+			for (var y = 0; y < height; ++y)
+				if (bitmap[x][y] === 0) {
 
 					context.fillStyle = "#f00"
 					context.fillRect(x * scale, y * scale, scale, scale)
 				}
-			}
-		}
 	}
 
-	brot()
+	setInterval(function() {
+
+		brot()
+		zoom *= 1.05
+
+	}, 50)
 }
